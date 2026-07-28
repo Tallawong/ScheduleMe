5f43dc9e167fbb732fa29ac5dbcf2188f839fa82
Janusz <rejkid@gmail.com>
2026-07-28 16:46:17 +1000

    Multi project build

--- scheduleme.client/src/app/account.controls/account.routes.ts
+++ scheduleme.client/src/app/account.controls/account.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { RegisterDialogComponent } from './register-dialog.component';
+import { VerifyEmailComponent } from './verify-email.component';
+import { ForgotPasswordComponent } from './forgot-password.component';
+import { ResetPasswordComponent } from './reset-password.component';
 
 export const ACCOUNT_ROUTES: Routes = [
   {
@@ -11,5 +14,8 @@ export const ACCOUNT_ROUTES: Routes = [
     component: RegisterDialogComponent,
     data: { title: 'Create Account', message: 'Sign up to get started' },
   },
+  { path: 'verify-email', component: VerifyEmailComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
